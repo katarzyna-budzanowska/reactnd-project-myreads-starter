bb85225fb78317229fb6567a766c6c8e995fef58
katarzyna-budzanowska <katarzyna.budzanowska@gmail.com>
2018-07-21 23:19:24 +0200

    Extract shelf.

---
+++ src/Shelf.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Book from './Book'
+
+class Shelf extends React.Component{
+  render() {
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{this.props.shelf}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+              { this.props.books.map( book => (
+                <li key={book.title} >
+                  <Book
+                    title={book.title}
+                    authors={book.authors}
+                    coverImage={book.imageLinks.smallThumbnail}
+                  />
+                </li>
+                ) )
+              }
+          </ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Shelf;
